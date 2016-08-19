39944a9eb0c7c70fd5326e0ccc078a41660a6d96
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-08-19 22:51:44 +0530

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Matrix
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader br= new BufferedReader(new InputStreamReader (System.in));
+		int m = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		char[][] a= new char[m][n];
+		String[] s;
+		
+		for(int i=0; i<m; i++){
+		    s = br.readLine().split(" ");
+		    
+		    for (int j=0; j<n; j++) {
+                a[i][j] = 
+		    }
+		    
+		}
+		
+		S
+	}
+}
